d0cae5a9abbdd6b1c82e576b8a89a17d79cd1bc7
ivanganchev <ivan_ganchev45@abv.bg>
2020-03-26 21:52:21 +0200

    Added search bar and horizontal upwards movement of it, removed default action bar

--- Fridger/app/src/main/java/com/example/fridger/MainActivity.java
+++ Fridger/app/src/main/java/com/example/fridger/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.fridger;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.graphics.Path;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SearchView searchBar;
+    ObjectAnimator animator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchBar = (SearchView) findViewById(R.id.searchBar);
+
+        searchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                horizontalUpMove(v, -350);
+            }
+        });
+
     }
+
+    private void horizontalUpMove(View view, float value) {
+        searchBar.setIconified(false);
+        animator = ObjectAnimator.ofFloat(view, "translationY", value);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setDuration(2000);
+        animator.start();
+    }
+
 }
